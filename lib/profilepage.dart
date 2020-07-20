0d5a85b8a8ad9e04d954aec6d2934145bd37440c
harshil30401 <harshilpatel30401@gmail.com>
2020-07-20 15:07:08 +0530

    Working code

--- lib/profilepage.dart
+++ lib/profilepage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:login_app/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:login_app/main.dart';
 import 'package:login_app/Widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
